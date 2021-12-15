9e14d1fb5e4ff852c7ddb9e4d215d8e869db434c
Alex Shepel <iam.alex.shepel@gmail.com>
2021-12-15 12:21:32 +0200

    fix render on load more

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -21,33 +21,15 @@ const App = () => {
   const [backupView, setBackupView] = useState('');
   const [modalImageURL, setModalImageURL] = useState('');
 
-  useEffect(() => {
-    const promiseCallback = () => pixabay.fetch();
-    const thenCallback = images => {
-      setImages(images);
-      setView(View.NORMAL);
-    };
-
-    handleLoading(promiseCallback, thenCallback);
-  }, []);
-
   const handleSubmit = query => {
     const promiseCallback = () => pixabay.fetch(query);
-    const thenCallback = images => {
-      setImages(images);
-      setView(View.NORMAL);
-    };
-
+    const thenCallback = images => setImages(images);
     handleLoading(promiseCallback, thenCallback);
   };
 
   const handleLoadMore = () => {
     const promiseCallback = pixabay.nextPage;
-    const thenCallback = images => {
-      setImages(prev => [...prev.images, ...images]);
-      setView(View.NORMAL);
-    };
-
+    const thenCallback = images => setImages(prev => [...prev, ...images]);
     handleLoading(promiseCallback, thenCallback);
   };
 
@@ -87,6 +69,12 @@ const App = () => {
     setView(View.MODAL);
   };
 
+  useEffect(() => {
+    const promiseCallback = () => pixabay.fetch();
+    const thenCallback = images => setImages(images);
+    handleLoading(promiseCallback, thenCallback);
+  }, []);
+
   return (
     <main className={s.App}>
       <Searchbar onSubmit={handleSubmit} />
@@ -106,108 +94,5 @@ const App = () => {
     </main>
   );
 };
-
-// class App extends Component {
-//   state = {
-//     images: [],
-//     view: View.LOADING,
-//     backupView: '',
-//     modalImageURL: '',
-//   };
-//
-//   componentDidMount() {
-//     const promiseCallback = () => pixabay.fetch();
-//     const thenCallback = images => this.setState({ images, view: View.NORMAL });
-//
-//     this.handleLoading(promiseCallback, thenCallback);
-//   }
-//
-//   handleSubmit = query => {
-//     const promiseCallback = () => pixabay.fetch(query);
-//     const thenCallback = images => this.setState({ images, view: View.NORMAL });
-//
-//     this.handleLoading(promiseCallback, thenCallback);
-//   };
-//
-//   handleLoadMore = () => {
-//     const promiseCallback = pixabay.nextPage;
-//     const thenCallback = images => {
-//       this.setState(prevState => ({
-//         images: [...prevState.images, ...images],
-//         view: View.NORMAL,
-//       }));
-//     };
-//
-//     this.handleLoading(promiseCallback, thenCallback);
-//   };
-//
-//   handleLoading = (promiseCallback, thenCallback) => {
-//     this.setState(prevState => {
-//       return {
-//         backupView: prevState.view,
-//         view: View.LOADING,
-//       };
-//     });
-//
-//     promiseCallback()
-//       .then(thenCallback)
-//       .then(() => this.setState({ view: View.NORMAL }))
-//       .catch(this.handleFetchError);
-//   };
-//
-//   handleFetchError = err => {
-//     if (err.message === pixabay.Messages.COLLECTION_END) {
-//       this.setState({ view: View.END });
-//       return;
-//     }
-//
-//     if (err.message === pixabay.Messages.NO_MATCHES) {
-//       this.setState({
-//         images: [],
-//         view: View.EMPTY,
-//       });
-//       return;
-//     }
-//
-//     console.log(err.message);
-//     this.setState(prevState => ({ view: prevState.backupView }));
-//   };
-//
-//   handleModalClose = () => {
-//     this.setState(prevState => ({ view: prevState.backupView }));
-//   };
-//
-//   handleItemClick = id => {
-//     this.setState(prevState => ({
-//       backupView: prevState.view,
-//       modalImageURL: prevState.images.find(img => img.id === id).largeImageURL,
-//       view: View.MODAL,
-//     }));
-//   };
-//
-//   render() {
-//     const { images, view, modalImageURL } = this.state;
-//     const { LOADING, NORMAL, END, MODAL, EMPTY } = View;
-//
-//     return (
-//       <main className={s.App}>
-//         <Searchbar onSubmit={this.handleSubmit} />
-//         <ImageGallery onItemClick={this.handleItemClick} images={images} />
-//
-//         {view === EMPTY && <p>There are no matching images.</p>}
-//
-//         {view === NORMAL && <Button onClick={this.handleLoadMore} />}
-//
-//         {view === END && <p>You've reached the collection end.</p>}
-//
-//         {view === LOADING && <ModalLoader color={'#3f51b5'} />}
-//
-//         {view === MODAL && (
-//           <Modal url={modalImageURL} onClose={this.handleModalClose} />
-//         )}
-//       </main>
-//     );
-//   }
-// }
 
 export default App;
