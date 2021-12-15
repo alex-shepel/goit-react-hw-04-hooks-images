d8b266a7da94db9c67436ade5bb441783e1d876a
Alex Shepel <iam.alex.shepel@gmail.com>
2021-12-15 12:49:23 +0200

    fix loader ref

--- src/components/ModalLoader/ModalLoader.jsx
+++ src/components/ModalLoader/ModalLoader.jsx
@@ -3,17 +3,18 @@ import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import { createPortal } from 'react-dom';
 import s from './ModalLoader.module.css';
 import PropTypes from 'prop-types';
-
-const loaderRef = document.querySelector('#loader');
+import { useRef } from 'react';
 
 const ModalLoader = ({ color = '#fff' }) => {
+  const loaderRef = useRef(document.querySelector('#loader'));
+
   const loader = (
     <div className={s.loader}>
       <Loader type="Oval" color={color} height={100} width={100} timeout={0} />
     </div>
   );
 
-  return createPortal(loader, loaderRef);
+  return createPortal(loader, loaderRef.current);
 };
 
 ModalLoader.propTypes = {
